cdd2288e6c829b71cb95e773d64e4becc116ea36
Richard Dodd <richard.dodd@itp-group.co.uk>
2018-02-27 12:17:06 +0000

    Fix example

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,4 +1,4 @@
-extern crate text_table;
+extern crate text_tables;
 
 use std::str;
 use std::io;
@@ -8,8 +8,8 @@ fn main() {
     let data = [["A", "2x2"], ["pretty", "table"]];
     // we can either render to an array...
     let mut out = Vec::new();
-    text_table::render(&mut out, data).unwrap();
+    text_tables::render(&mut out, data).unwrap();
     println!("{}", str::from_utf8(&out).unwrap());
     // ...or we can use `Write` streams directly
-    text_table::render(&mut io::stdout(), data).unwrap();
+    text_tables::render(&mut io::stdout(), data).unwrap();
 }
